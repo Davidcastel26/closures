d8fafe5bc42b4a54bd1d4a9339f68375d2d33b2b
David Castellanos <davcastellanoslarios@gmail.com>
2022-10-23 17:05:23 -0600

    Update closureWithObj.js

---
+++ closureWithObj.js
@@ -0,0 +1,25 @@
+function createCounter(){
+    //setting our variable counter as 0
+    let counter = 0;
+    //return will be an object but 
+    return {
+        increase: function(){
+            counter++;
+            return counter;
+        },
+        decrease: function(){
+            counter--;
+            return counter;
+        },
+        getValue: function(){
+            return contador;
+        }
+    }
+}
+
+const counter1 = createCounter()
+counter1.increase();
+counter1.increase();
+counter1.getValue()
+counter1.decrease();
+counter1.getValue();
