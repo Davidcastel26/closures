7f4434897fa97d5beb85056df5417f7c7ff98ddf
David Castellanos <davcastellanoslarios@gmail.com>
2022-10-23 18:12:03 -0600

    Update js&cssClosures.js

--- js&cssClosures.js
+++ js&cssClosures.js
@@ -1,8 +1,11 @@
+
 function createMsg(typeMsg, styles){
     return function(str){
-        console.log(`%c ${typeMsg}: ${str}`, styles)
+        // console.log(`%c ${typeMsg}: ${str}`, styles)
+        console.log(`%c ${typeMsg}: ${str}`, styleByDeffault + styles)
     }
 }
 
-const error= createMsg('Error', 'background: red; color:white')
+// const error= createMsg('Error', 'background: red; color:white')
+const error= createMsg('Error', 'background: red;')
 error('Account not found')
