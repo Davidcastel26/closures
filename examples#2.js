6516d28f36da6f3bff42dc4694acabb4f53f1f56
David Castellanos <davcastellanoslarios@gmail.com>
2022-10-24 18:57:12 -0600

    Update examples#2.js

--- examples#2.js
+++ examples#2.js
@@ -20,4 +20,16 @@ newCounter2 = counter();// Nueva variable counter
 
 // ---------------------------------------------------------------
 // ejercicio 2
+// usa closures para crear un cache para la function
+//la funcion que retornas debe aceptar un solo argumento e invocar a cb con ese argumento
+//cuando la function que hayas retornado es invocada e nuevo, eberias guarar el argumento y el resultao
+//cuao la funciton que retornaste sea invocaa e nuevo con un argumento con el cual se habia invocao 
+// ejemplo:
+//cb -> fuction(x){return x * x}
+//si invocas la function que retornaste con 5, aentro eberia invocar cb(5), aentro y retornar 25
+// si la invocas e nuevo con 5, eberia retornar 25 (guarao previamente en el cache)
+//tips, usa un objeto one caa propiea sea un argumento, y el valor el resultao.
+// usa hasOwnProperty!
+function cacheFunction(){
 
+}
